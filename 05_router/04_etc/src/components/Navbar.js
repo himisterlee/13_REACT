302e06db3e221a0e7f1f02edab1fb1361c6115ec
himisterlee <lejlej100418@gmail.com>
2024-08-28 14:51:38 +0900

    수정

--- 05_router/04_etc/src/components/Navbar.js
+++ 05_router/04_etc/src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
             <ul>
                 <li><NavLink to='/' style={({isActive}) => isActive? activeStyle : undefined}>Home</NavLink></li>
                 <li><NavLink to='/login' style={({isActive}) => isActive? activeStyle : undefined}>로그인</NavLink></li>
-                <li><NavLink to='mypage' style={({isActive}) => isActive? activeStyle : undefined}>마이페이지</NavLink></li>
+                <li><NavLink to='/mypage' style={({isActive}) => isActive? activeStyle : undefined}>마이페이지</NavLink></li>
             </ul>
         </div>
     )
